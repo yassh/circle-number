4530151f240fcc3a2d8d6b7adec172b63ed1d10e
yassh <yassh32@outlook.jp>
2016-07-27 01:24:38 +0900

    add help

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,11 @@
 'use strict';
 
 const circleNumber = require('../');
-const argv = require('yargs').argv;
+const argv = require('yargs')
+  .help('help').alias('help', 'h')
+  .usage('Usage: $0 <number ...>')
+  .example('$0 3 12 35', 'print circled 3, circled 12, and circled 35')
+  .argv;
 
 argv._.forEach((n) => {
   console.log(circleNumber(n));
